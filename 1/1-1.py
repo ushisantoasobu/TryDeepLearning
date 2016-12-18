dd98ebaf150879ef928335614289e85a0cbfb152
Shunsuke Sato <shunsukesato@ShunsukenoMacBook-Pro.local>
2016-12-18 21:46:30 +0900

    chapter 1

---
+++ 1/1-1.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+x = np.array([1, 2, 3])
+print(x)
+
+y = np.array([2, 4, 6])
+print(x + y)
+print(x * y)
+print(x / y)
+
+# z = np.array([100, 200])
+# print(x + z) #要素数が異なるのでエラー
---
+++ 1/1-4.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+from matplotlib.image import imread
+
+img = imread('./sniff.png')
+plt.imshow(img)
+
+plt.show()
+
+
