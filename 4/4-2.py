521b0d3ee6139c36b52a9757b3df52d7ab257678
ushisantoasobu <babblemann.shunsee@gmail.com>
2016-12-22 22:57:29 +0900

    chapter 4 損失関数 基礎

---
+++ 4/4-2.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# 交差エントロピー誤差
+def cross_entropy_error(y, t):
+    delta = 1e-7
+    return -np.sum(t * np.log(y + delta))
+
+t = [0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
+y = [0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0]
+print(cross_entropy_error(np.array(y), np.array(t)))
+
+y = [0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.5, 0.0]
+print(cross_entropy_error(np.array(y), np.array(t)))
