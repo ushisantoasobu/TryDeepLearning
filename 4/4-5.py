f3322e0889257b0b6f1a690bb8489250da9deabc
ushisantoasobu <babblemann.shunsee@gmail.com>
2016-12-24 13:24:53 +0900

    chapter 4 勾配

---
+++ 4/4-5.py
@@ -0,0 +1,28 @@
+# coding: utf-8
+import numpy as np
+
+def function_2(x):
+    return x[0]**2 + x[1]**2
+
+def numerical_gradient(f, x):
+    h = 1e-4
+    grad = np.zeros_like(x) # xと同じ形状の配列を作成
+
+    for idx in range(x.size):
+        tmp_val = x[idx]
+        # f(x+h)の計算
+        x[idx] = tmp_val + h
+        fxh1 = f(x)
+
+        # f(x-h)の計算
+        x[idx] = tmp_val - h
+        fxh2 = f(x)
+
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+        x[idx] = tmp_val # 元の値に戻す
+
+    return grad
+
+print(numerical_gradient(function_2, np.array([3.0, 4.0])))
+print(numerical_gradient(function_2, np.array([0.0, 2.0])))
+print(numerical_gradient(function_2, np.array([3.0, 0.0])))
