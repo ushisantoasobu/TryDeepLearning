8adfbabde411ad50c1959b02db3ba5156f2c7f57
ushisantoasobu <babblemann.shunsee@gmail.com>
2016-12-31 18:36:03 +0900

    chapter 5 誤差逆伝搬法 基本

---
+++ 5/5-1.py
@@ -0,0 +1,63 @@
+class MultiLayer:
+    def __init__(self):
+        self.x = None
+        self.y = None
+
+    def forward(self, x, y):
+        self.x = x
+        self.y = y
+        out = x * y
+
+        return out
+
+    def backward(self, dout):
+        dx = dout * self.y
+        dy = dout * self.x
+
+        return dx, dy
+
+class AddLayer:
+    def __init__(self):
+        pass
+
+    def forward(self, x, y):
+        out = x + y
+
+        return out
+
+    def backward(self, dout):
+        dx = dout * 1
+        dy = dout * 1
+
+        return dx, dy
+
+### example 1
+
+apple = 100
+apple_num = 2
+orange = 150
+orange_num = 3
+tax = 1.1
+
+#layer
+mul_apple_layer = MultiLayer()
+mul_orange_layer = MultiLayer()
+mul_apple_orange_layer = AddLayer()
+mul_tax_layer = MultiLayer()
+
+#forward
+apple_price = mul_apple_layer.forward(apple, apple_num)
+orange_price = mul_orange_layer.forward(orange, orange_num)
+all_price = mul_apple_orange_layer.forward(apple_price, orange_price)
+price = mul_tax_layer.forward(all_price, tax)
+
+print(price)
+
+# backward
+d_price = 1
+dall_price, dtax = mul_tax_layer.backward(d_price)
+dapple_price, dorange_price = mul_apple_orange_layer.backward(dall_price)
+dapple, dapple_num = mul_apple_layer.backward(dapple_price)
+dorrange, dorrange_num = mul_orange_layer.backward(dorange_price)
+
+print(dapple, dapple_num, dorrange, dorrange_num, dtax)
